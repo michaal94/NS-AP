7ec9fdebd95ee801402b291b1dabd8c8ae4fd023
michaal94 <michaal94@gmail.com>
2022-09-29 12:44:42 +0100

    msg

--- model/inference_eqa.py
+++ model/inference_eqa.py
@@ -2317,7 +2317,7 @@ class InferenceToolDebug:
         ori = obs['robot0_eef_quat']
         # print(pos, ori)
         des_pos = pos + action[0:3]
-        # print(pos, des_pos)
+        print(pos, des_pos)
         des_ori = T.quat_multiply(ori, T.axisangle2quat(action[3:6]))
         # print(des_pos)
         shift_to_wrist = np.matmul(T.quat2mat(des_ori), np.array([0, 0, -0.103]))
